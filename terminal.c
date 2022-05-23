b3074f1f948b91886dcab5d86e8a560c30633981
Lennon McLean <lmmclean08@gmail.com>
2022-05-23 07:52:39 -0400

    terminal multiplexing

--- terminal.c
+++ terminal.c
@@ -1,35 +1,27 @@
-#include "io.h"
+#include "keyboard.h"
+#include "vga.h"
+#include "serial.h"
+
+int tty = 1;
 
 void scan(char* buffer) {
-    int index = 0;
-    while(1) {
-        if (in(0x03fd) % 2) {
-            unsigned char inByte = in(0x03f8);
-            if (inByte == '\r') {
-                buffer[index] = 0;
-                out(0x03f8, '\r');
-                out(0x03f8, '\n');
-                return;
-            }
-            else if (inByte == '\b') {
-                if (index > 0) {
-                    index--;
-                    out(0x03f8, '\b');
-                    out(0x03f8, ' ');
-                    out(0x03f8, '\b');
-                }
-            }
-            else {
-                buffer[index] = inByte;
-                out(0x03f8, buffer[index]);
-                index++;
-            }
-        }
+    if (tty) {
+        keyboardScan(buffer);
+    }
+    else {
+        serialScan(buffer);
     }
 }
 
 void print(char* string) {
-    for (int i = 0; string[i] != 0; i++) {
-        out(0x03f8, string[i]);
+    if (tty) {
+        vgaPrint(string);
     }
+    else {
+        serialPrint(string);
+    }
+}
+
+void ctty(int newTty) {
+    tty = newTty;
 }
--- terminal.h
+++ terminal.h
@@ -1,2 +1,3 @@
 void scan(char* buffer);
 void print(char* string);
+void ctty(int newTty);
