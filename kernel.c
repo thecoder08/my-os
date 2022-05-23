04f77c238ccbe64f3432fe3e5a288f28b823142e
Lennon McLean <lmmclean08@gmail.com>
2022-05-23 07:34:20 -0400

    kernel supports backspace

--- kernel.c
+++ kernel.c
@@ -6,17 +6,20 @@
 void kmain() {
       char* input = malloc(100);
       clear(' ', 0x07);
-      vgaPrint("Welcome to My OS! Use the \"help\" command for help. Loading modules from disk isn't supported (yet) so only preloaded modules are usable.\r\n");
+      vgaPrint("Welcome to My OS! Use the \"help\" command for help. Loading programs from disk isn't yet supported so only builtin commands are supported,\r\n");
       while (1) {
             vgaPrint("> ");
             keyboardScan(input);
             if (strcmp(input, "help")) {
-                  vgaPrint("clear: clear the screen");
-                  vgaPrint("My OS is based on the concept of modules. Modules are independent programs that run in ring-0. This means that they have full access to the hardware. Modules register themselves with the kernel to provide a command the user can run.\r\n");
-                  vgaPrint("Loaded modules:\r\n");
+                  vgaPrint("help: print this message.\r\n");
+                  vgaPrint("clear: clear the screen.\r\n");
+                  vgaPrint("halt: stop the OS.\r\n");
             }
-            if (strcmp(input, "clear")) {
+            else if (strcmp(input, "clear")) {
                   clear(' ', 0x07);
+            }
+            else if (strcmp(input, "halt")) {
+                  while(1);
             }
             else {
                   vgaPrint("I'm not sure what \"");
