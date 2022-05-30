8e01f46705cb9acc239caf7afefe32d54243f73a
Lennon McLean <lmmclean08@gmail.com>
2022-05-30 10:13:38 -0400

    Update serial.c

--- serial.c
+++ serial.c
@@ -1,5 +1,11 @@
 #include "io.h"
 
+void serialPrint(char* string) {
+    for (int i = 0; string[i] != 0; i++) {
+        out(0x03f8, string[i]);
+    }
+}
+
 void serialScan(char* buffer) {
     int index = 0;
     while(1) {
@@ -7,16 +13,13 @@ void serialScan(char* buffer) {
             unsigned char inByte = in(0x03f8);
             if (inByte == '\r') {
                 buffer[index] = 0;
-                out(0x03f8, '\r');
-                out(0x03f8, '\n');
+                serialPrint("\r\n");
                 return;
             }
             else if (inByte == '\b') {
                 if (index > 0) {
                     index--;
-                    out(0x03f8, '\b');
-                    out(0x03f8, ' ');
-                    out(0x03f8, '\b');
+                    serialPrint("\b \b");
                 }
             }
             else {
@@ -25,11 +28,5 @@ void serialScan(char* buffer) {
                 index++;
             }
         }
-    }
-}
-
-void serialPrint(char* string) {
-    for (int i = 0; string[i] != 0; i++) {
-        out(0x03f8, string[i]);
     }
 }
